e660eb0492526c1ae7bb7cc37e05374e7eeaf170
goroguz <gmgkorohova@gmail.com>
2025-07-16 23:04:14 +0300

    added softAssertions for 2 tests to support mulitple asserts in one test.

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,3 +1,4 @@
+import org.assertj.core.api.SoftAssertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -39,8 +40,14 @@ public class BurgerTest {
     @Test
     public void testAddIngredient() {
         burger.addIngredient(cheese);
-        assertEquals(1, burger.ingredients.size());
-        assertEquals(cheese, burger.ingredients.get(0));
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(burger.ingredients.size())
+            .as("Проверка размера списка ингредиентов")
+            .isEqualTo(1);
+        softly.assertThat(burger.ingredients.get(0))
+            .as("Проверка добавленного ингредиента")
+            .isEqualTo(cheese);
+        softly.assertAll();
     }
 
     @Test
@@ -55,7 +62,16 @@ public class BurgerTest {
         burger.addIngredient(cheese);
         burger.addIngredient(cutlet);
         burger.moveIngredient(0, 1);
-        assertEquals(cutlet, burger.ingredients.get(0));
-        assertEquals(cheese, burger.ingredients.get(1));
+        SoftAssertions softly = new SoftAssertions();
+
+        softly.assertThat(burger.ingredients.get(0))
+            .as("Ингредиент на позиции 0 должен быть cutlet")
+            .isEqualTo(cutlet);
+
+        softly.assertThat(burger.ingredients.get(1))
+            .as("Ингредиент на позиции 1 должен быть cheese")
+            .isEqualTo(cheese);
+
+        softly.assertAll();
     }
 }
